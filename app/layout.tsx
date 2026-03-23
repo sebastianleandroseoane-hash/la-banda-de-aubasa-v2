af2abc7a70370017e9a4122ebe698aa87e11a683
sebastianleandroseoane-hash <sebastianleandroseoane@gmail.com>
2026-03-23 07:19:24 -0300

    analistic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
